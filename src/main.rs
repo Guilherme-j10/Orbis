811eee03a97802650db7d82243f404f5dc6180aa
Guilherme-j10 <guilhermeZA.123123@gmail.com>
2026-06-17 17:18:09 -0300

    Add rendering functions for file system and font editor in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use std::sync::Arc;
+use std::{f32::consts::PI, sync::Arc};
 
-use femtovg::{Canvas, Color};
-use winit::{event::WindowEvent, window::Window};
+use femtovg::{Canvas, Color, Paint, Path, Renderer};
+use winit::{dpi::PhysicalSize, event::WindowEvent, window::Window};
 
 use crate::wgpu::{Callbacks, WindowSurface};
 
@@ -9,6 +9,24 @@ mod wgpu;
 
 fn main() {
     wgpu::start_wgpu(1440, 900, "Orbis", false);
+}
+
+fn file_system_container<T: Renderer>(canvas: &mut Canvas<T>, size: &PhysicalSize<u32>) -> () {
+    let mut main_container = Path::new();
+    main_container.rect(0.0, 0.0, 320.0, size.height as f32);
+    canvas.fill_path(&main_container, &Paint::color(Color::rgb(46, 52, 61)));
+}
+
+fn font_editor<T: Renderer>(canvas: &mut Canvas<T>, size: &PhysicalSize<u32>) -> () {
+    let mut main_container = Path::new();
+    main_container.rect(320.0, 0.0, size.width as f32, size.height as f32);
+    canvas.fill_path(&main_container, &Paint::color(Color::rgb(40, 43, 51)));
+
+    //draw mask
+
+    let mut circle_path = Path::new();
+    circle_path.arc(340.0, 20.0, 20.0, 0.0, PI * 2.0, femtovg::Solidity::Solid);
+    canvas.stroke_path(&circle_path, &Paint::color(Color::rgb(255, 0, 0)).with_line_width(3.0));
 }
 
 fn run<W: WindowSurface + 'static>(
@@ -20,13 +38,16 @@ fn run<W: WindowSurface + 'static>(
         window_event: Box::new(move |event, event_loop| match event {
             WindowEvent::Resized(physical_size) => {
                 surface.resize(physical_size.width, physical_size.height);
-            },
+            }
             WindowEvent::RedrawRequested => {
                 let dpi_factor = window.scale_factor();
                 let size = window.inner_size();
 
                 canvas.set_size(size.width, size.height, dpi_factor as f32);
-                canvas.clear_rect(0, 0, size.width, size.height, Color::rgb(0, 0, 0));
+                canvas.clear_rect(0, 0, size.width, size.height, Color::rgb(40, 43, 51));
+
+                file_system_container(&mut canvas, &size);
+                font_editor(&mut canvas, &size);
 
                 surface.present(&mut canvas);
             }
